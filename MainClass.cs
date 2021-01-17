c90fb4cc646fb4fe9f30435c630a29f395f3256b
Sean Duggan <sjdsoundworks@gmail.com>
2021-01-17 15:10:21 +0000

    Add project files.

---
+++ MainClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MordhauSuggestionGenerator
+{
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many Insightful Veteran Suggestions would you like to generate?");
+            string r = Console.ReadLine();
+            try
+            {
+                int amount = int.Parse(r);
+                for (int i = 0; i < amount; i++)
+                {
+                    BullshitBuilder b = new BullshitBuilder();
+                    Console.WriteLine(b.WriteShitpost());
+                }
+            } catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e + "\n write a number, idiot, that's all you have to do.");
+            }
+        }
+    }
+}
